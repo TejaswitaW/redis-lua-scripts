f9c6f2b2c375ff81e03fc7220fa80375f4b5dd9a
tejaswitaw <31151862+TejaswitaW@users.noreply.github.com>
2024-11-01 16:08:25 +0530

    strings in lua

---
+++ input.lua
@@ -0,0 +1,10 @@
+a = tostring(20)
+print(type(a))
+name = io.read()
+print("Your name is: ",name)
+--[[
+string
+tina
+Your name is:   tina
+]]
+-- print("test"+2) error
---
+++ strings.lua
@@ -0,0 +1,21 @@
+a = "John"
+html = [[
+<h1>Welcome</h1>
+<h2>Services</h2>
+]]
+print(html)
+print("10"+2)
+print(20 .. 40)
+x = 'one string'
+y = string.gsub(x,'one','two')
+print(x)
+print(y)
+--[[
+<h1>Welcome</h1>
+<h2>Services</h2>
+
+12.0
+2040
+one string
+two string
+]]
