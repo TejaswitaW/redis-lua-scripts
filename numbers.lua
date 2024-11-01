7c591c39c0b0a648038ed39c54876af5ce5e5e58
tejaswitaw <31151862+TejaswitaW@users.noreply.github.com>
2024-11-01 08:28:31 +0530

    number data type in lua

---
+++ numbers.lua
@@ -0,0 +1,13 @@
+n1 = 4
+n2 = 0.4
+n3 = 4.57
+print(n1,n2,n3)
+print("Type n1: ",type(n1))
+print("Tyep n2: ",type(n2))
+print("Type n3: ",type(n3))
+--[[
+4       0.4     4.57
+Type n1:        number
+Tyep n2:        number
+Type n3:        number
+]]
